fa6a5fa1370d2c4c5b1f57af02fd5b431bb9049d
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-04 11:07:20 +0100

    initial graphql and apollo server code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+
+//got rid of routes
+
+const { ApolloServer } = require("apollo-server");
+const db = require("./config/connection");
+
+//need to require resolvers, typedefs and context
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +15,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+});
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(`🌍 Now listening on https://localhost:${PORT}`)
+  );
+});
+
+db.once("open", () => {
+  server.listen().then(({ url }) => {
+    console.log(`🌍 Server ready at ${url}`);
+  });
 });
