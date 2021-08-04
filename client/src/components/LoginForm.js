2f8c1d2b72bd618c6339aefb70646c0d3f64eba1
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-04 21:17:14 +0100

    loginform frontend

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -39,15 +39,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      await login({
+        variables: {
+          loginInput: userFormData,
+        },
+      });
     } catch (err) {
       console.error(err);
       setShowAlert(true);
